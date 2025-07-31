0fdddb0bb5d8d243ecec0effdb58ec178b8e275a
Nurcan Kurtoğlu <nurcankurtoglu100@gmail.com>
2025-07-31 18:28:27 +0300

    background.js

--- background.js
+++ background.js
@@ -20,4 +20,51 @@ function resize() {
   canvas.width = width;
   canvas.height = height;
 }
-window.addEventListener('resize', resize
+window.addEventListener('resize', resize);
+resize();
+
+class Star {
+  constructor() {
+    this.x = Math.random() * width;
+    this.y = Math.random() * height;
+    this.radius = Math.random() * 1.5 + 0.3;
+    this.alpha = Math.random();
+    this.alphaChange = (Math.random() * 0.02 + 0.005) * (Math.random() < 0.5 ? 1 : -1);
+  }
+  update() {
+    this.alpha += this.alphaChange;
+    if (this.alpha <= 0) {
+      this.alpha = 0;
+      this.alphaChange = -this.alphaChange;
+    } else if (this.alpha >= 1) {
+      this.alpha = 1;
+      this.alphaChange = -this.alphaChange;
+    }
+  }
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.fillStyle = `rgba(255, 255, 255, ${this.alpha})`;
+    ctx.shadowColor = 'rgba(255,255,255,0.8)';
+    ctx.shadowBlur = 5;
+    ctx.fill();
+  }
+}
+
+const starsCount = 120;
+const stars = [];
+
+for(let i=0; i<starsCount; i++) {
+  stars.push(new Star());
+}
+
+function animate() {
+  ctx.clearRect(0,0,width,height);
+  stars.forEach(star => {
+    star.update();
+    star.draw();
+  });
+  requestAnimationFrame(animate);
+}
+
+animate();
