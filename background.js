c71b82c1adf551c956f12b4fbf7a99fa2a37fc87
Nurcan Kurtoğlu <nurcankurtoglu100@gmail.com>
2025-07-31 17:54:52 +0300

    background.js
    Düzenlendi.

--- background.js
+++ background.js
@@ -6,7 +6,7 @@ let stars = [];
 
 function init() {
   resize();
-  createStars(120);
+  createStars(150);
   animate();
 }
 
@@ -17,13 +17,35 @@ function resize() {
   canvas.height = height;
 }
 
-window.addEventListener('resize', () => {
-  resize();
-});
+window.addEventListener('resize', resize);
 
 function createStars(count) {
   stars = [];
-  for (let i = 0; i < count; i++) {
+  for(let i=0; i < count; i++) {
     stars.push({
-      x:
+      x: Math.random() * width,
+      y: Math.random() * height,
+      radius: Math.random() * 1.2 + 0.5,
+      alpha: Math.random(),
+      alphaChange: (Math.random() * 0.02) + 0.005
+    });
+  }
+}
 
+function animate() {
+  ctx.clearRect(0, 0, width, height);
+  for(let star of stars) {
+    star.alpha += star.alphaChange;
+    if(star.alpha <= 0 || star.alpha >= 1) star.alphaChange *= -1;
+
+    ctx.beginPath();
+    ctx.arc(star.x, star.y, star.radius, 0, 2 * Math.PI);
+    ctx.fillStyle = `rgba(255, 255, 255, ${star.alpha})`;
+    ctx.shadowColor = 'rgba(255, 255, 255, 0.7)';
+    ctx.shadowBlur = 8;
+    ctx.fill();
+  }
+  requestAnimationFrame(animate);
+}
+
+init();
