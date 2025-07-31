93eb85dcfadb4d4b291f32cfd402bcd35680bd27
Nurcan Kurtoğlu <nurcankurtoglu100@gmail.com>
2025-07-31 19:26:17 +0300

    background.js

--- background.js
+++ background.js
@@ -1,34 +1,48 @@
-/* Dynamic Stars Background */
+const canvas = document.getElementById("background");
+const ctx = canvas.getContext("2d");
 
-#stars, #stars2, #stars3 {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-repeat: repeat;
-  z-index: 0;
+let stars = [];
+const STAR_COUNT = 200;
+
+function resizeCanvas() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+window.addEventListener("resize", resizeCanvas);
+resizeCanvas();
+
+for (let i = 0; i < STAR_COUNT; i++) {
+  stars.push({
+    x: Math.random() * canvas.width,
+    y: Math.random() * canvas.height,
+    radius: Math.random() * 1.5,
+    speed: Math.random() * 0.5 + 0.2
+  });
 }
 
-#stars {
-  background: url('https://raw.githubusercontent.com/VincentGarreau/particles.js/master/demo/media/star.png');
-  animation: moveStars 200s linear infinite;
-  opacity: 0.4;
+function drawStars() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "#ffffff";
+  for (let star of stars) {
+    ctx.beginPath();
+    ctx.arc(star.x, star.y, star.radius, 0, Math.PI * 2);
+    ctx.fill();
+  }
 }
 
-#stars2 {
-  background: url('https://raw.githubusercontent.com/VincentGarreau/particles.js/master/demo/media/star.png');
-  animation: moveStars 400s linear infinite;
-  opacity: 0.3;
+function updateStars() {
+  for (let star of stars) {
+    star.y += star.speed;
+    if (star.y > canvas.height) {
+      star.y = 0;
+      star.x = Math.random() * canvas.width;
+    }
+  }
 }
 
-#stars3 {
-  background: url('https://raw.githubusercontent.com/VincentGarreau/particles.js/master/demo/media/star.png');
-  animation: moveStars 600s linear infinite;
-  opacity: 0.2;
+function animate() {
+  drawStars();
+  updateStars();
+  requestAnimationFrame(animate);
 }
-
-@keyframes moveStars {
-  from { transform: translateY(0); }
-  to { transform: translateY(-1000px); }
-}
+animate();
