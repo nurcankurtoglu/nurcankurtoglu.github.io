cc048e0a577730099b18669799f74d69e85b307c
Nurcan Kurtoğlu <nurcankurtoglu100@gmail.com>
2025-08-01 15:12:13 +0300

    background.js

--- background.js
+++ background.js
@@ -8,11 +8,10 @@ function resize() {
     canvas.width = width;
     canvas.height = height;
 }
-
 window.addEventListener('resize', resize);
 resize();
 
-// Basit kuzey ışıkları efekt örneği
+// Aurora partikülleri
 class AuroraParticle {
     constructor() {
         this.reset();
@@ -20,48 +19,19 @@ class AuroraParticle {
     reset() {
         this.x = Math.random() * width;
         this.y = Math.random() * height;
-        this.radius = Math.random() * 80 + 50;
+        this.radius = Math.random() * 100 + 60;
         this.alpha = 0.05 + Math.random() * 0.1;
-        this.dx = (Math.random() - 0.5) * 0.3;
-        this.dy = (Math.random() - 0.5) * 0.3;
+        this.dx = (Math.random() - 0.5) * 0.2;
+        this.dy = (Math.random() - 0.5) * 0.2;
         this.color = `hsla(${Math.random() * 140 + 180}, 80%, 70%, ${this.alpha})`;
     }
     update() {
         this.x += this.dx;
         this.y += this.dy;
-
         if (this.x < -this.radius) this.x = width + this.radius;
         else if (this.x > width + this.radius) this.x = -this.radius;
-
         if (this.y < -this.radius) this.y = height + this.radius;
         else if (this.y > height + this.radius) this.y = -this.radius;
     }
     draw() {
-        const gradient = ctx.createRadialGradient(this.x, this.y, 0, this.x, this.y, this.radius);
-        gradient.addColorStop(0, this.color);
-        gradient.addColorStop(1, 'transparent');
-
-        ctx.fillStyle = gradient;
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-        ctx.fill();
-    }
-}
-
-const particlesCount = 30;
-const particles = [];
-
-for(let i = 0; i < particlesCount; i++) {
-    particles.push(new AuroraParticle());
-}
-
-function animate() {
-    ctx.clearRect(0, 0, width, height);
-    for(let p of particles) {
-        p.update();
-        p.draw();
-    }
-    requestAnimationFrame(animate);
-}
-
-animate();
+        const gradient = ctx.createRadialGradient(this.x, this.y, 0, this
